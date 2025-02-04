ce0ac10ccaf7fcf249747c31151a68ddd9be482e
kevinmartinezc <ceronk003@gmail.com>
2025-02-03 21:24:34 -0600

    feat: map response to the movie model

--- core/actions/movies/now-playing.actions.ts
+++ core/actions/movies/now-playing.actions.ts
@@ -1,11 +1,12 @@
 import { movieApi } from "@/core/api/movie-api";
 import { MovieDBResponse } from "@/infrastructure/interfaces/moviedb-reponse";
+import { MovieMapper } from "@/infrastructure/mappers/movie.mappers";
 
 export const nowPLayingAction = async () => {
   try {
-    const {data} = await movieApi.get<MovieDBResponse>('/now_playing')
-    console.log(data);
-    return [];
+    const { data } = await movieApi.get<MovieDBResponse>("/now_playing");
+
+    return data.results.map(MovieMapper.fromTheMovieDBToMoview);
   } catch (error) {
     console.log(error);
     throw "Cannot load now playing movies";
