1694451f51d94337dfe7824d4af81c5f68018a49
kevinmartinezc <ceronk003@gmail.com>
2025-02-06 09:11:02 -0600

    fix: imports

--- core/actions/movies/movie/get-movie-by-id.action.ts
+++ core/actions/movies/movie/get-movie-by-id.action.ts
@@ -1,6 +1,6 @@
 import { movieApi } from "@/core/api/movie-api";
 import { CompleteMovie } from "@/infrastructure/interfaces/movie.interface";
-import { MovieDetailDBResponse } from "@/infrastructure/interfaces/moviedb-movie.response";
+import { MovieDetailDBResponse } from "@/infrastructure/interfaces/moviedb-detail.response";
 import { MovieMapper } from "@/infrastructure/mappers/movie.mappers";
 
 export const getMovieByIdAction = async (id: number | string): Promise<CompleteMovie> => {
